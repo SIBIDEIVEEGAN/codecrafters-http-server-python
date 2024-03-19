60edf0bc035b2e198e563e55e56d33a4907a1559
SIBIDEIVEEGAN <sibideiveegan@gmail.com>
2024-03-19 23:20:47 +0530

    multi request

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 # Uncomment this to pass the first stage
 import socket
+import threading
 
 
 def main():
@@ -10,8 +11,16 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     
-    con , adr = server_socket.accept() # wait for client
     
+    
+    while True:
+        con , adr = server_socket.accept() # wait for client
+        thread = threading.Thread(target=handle_client, args=(con, adr))
+        thread.start()
+
+
+
+def handle_client(con, adr):
     data = con.recv(1024).decode('utf-8').splitlines()
 
     path = data[0].split(" ")[1]
@@ -19,21 +28,17 @@ def main():
     if path == "/":
         con.send(b"HTTP/1.1 200 OK \r\n\r\n")
     elif path.startswith("/echo/"):
-        
         msg = path[len("/echo/"):]
         mlen = len(msg)
         con.send(f"HTTP/1.1 200 OK \r\nContent-Type: text/plain\r\nContent-Length: {mlen}\r\n\r\n{msg}".encode())
     elif path == "/user-agent":
         user_agent = data[2].split(":", 1)[1].strip()
-       
         mlen = len(user_agent)
         con.send(f"HTTP/1.1 200 OK \r\nContent-Type: text/plain\r\nContent-Length: {mlen}\r\n\r\n{user_agent}".encode())
-
     else:
         con.send(b"HTTP/1.1 404 Not Found \r\n\r\n")
 
-
-
+    con.close()
 
 if __name__ == "__main__":
     main()
