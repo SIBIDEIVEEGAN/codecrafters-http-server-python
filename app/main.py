3556feca5588616cb4d9fb17a57bd0d9d54aede2
SIBIDEIVEEGAN <sibideiveegan@gmail.com>
2024-03-19 22:24:14 +0530

    Respond with 404

--- app/main.py
+++ app/main.py
@@ -11,6 +11,20 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     
     con , adr = server_socket.accept() # wait for client
+    
+    data = con.recv(1024).decode('utf-8').splitlines()
+
+    path = data[0].split(" ")
+
+    if path[1] == "/":
+        con.send(b"HTTP/1.1 200 OK \r\n\r\n")
+    else:
+        con.send(b"HTTP/1.1 404 Not Found \r\n\r\n")
+
+
+
+
+    
     con.send(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
